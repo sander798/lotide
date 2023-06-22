d6b9460111bdedfb5abc3bfa81ad1f6e0756698d
Alexander Evans <sander7989@gmail.com>
2023-06-22 13:23:12 -0600

    add function for counting elements of array that correspond to object properties

---
+++ countOnly.js
@@ -0,0 +1,45 @@
+const assertEqual = function(actual, expected) {
+  
+  if (actual === expected) {
+    console.log(`:D Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`:( Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+const countOnly = function(allItems, itemsToCount) {
+  
+  const results = {};
+  
+  for (const i of allItems) {
+    
+    if (itemsToCount[i]){//If the item exists as a property of itemsToCount, count it
+      if (!results[i]) {
+        results[i] = 1;
+      } else {
+        results[i]++;
+      }
+    }
+  }
+  
+  return results;
+};
+
+const firstNames = [
+  "Karl",
+  "Salima",
+  "Agouhanna",
+  "Fang",
+  "Kavith",
+  "Jason",
+  "Salima",
+  "Fang",
+  "Joe"
+];
+
+const result1 = countOnly(firstNames, { "Jason": true, "Karima": true, "Fang": true, "Agouhanna": false });
+
+assertEqual(result1["Jason"], 1);
+assertEqual(result1["Karima"], undefined);
+assertEqual(result1["Fang"], 2);
+assertEqual(result1["Agouhanna"], undefined);
