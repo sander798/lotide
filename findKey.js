9873012427e3e2a906d0917fe16b414f78ffc1ea
Alexander Evans <sander7989@gmail.com>
2023-06-26 20:42:25 -0600

    add function to find key by value with callback function

---
+++ findKey.js
@@ -0,0 +1,32 @@
+const findKey = function(object, callback) {
+  for (let e of Object.keys(object)) {
+    if (callback(object[e])) {
+      return e;
+    }
+  }
+  
+  return undefined;
+};
+
+const assertEqual = function(actual, expected) {
+  
+  if (actual === expected) {
+    console.log(`:D Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`:( Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+console.log(findKey({
+  "Blue Hill": { stars: 1 },
+  "Akaleri":   { stars: 3 },
+  "noma":      { stars: 2 },
+  "elBulli":   { stars: 3 },
+  "Ora":       { stars: 2 },
+  "Akelarre":  { stars: 3 }
+}, x => x.stars === 2)); // => "noma"
+
+assertEqual(findKey({blah: 2, s: 1, dsf: 3}, x => x === 3), "dsf");
+assertEqual(findKey({a: "sadasd", b: 1, c: 3}, x => x === "sadasd"), "a");
+assertEqual(findKey({a: 2, b: 1, c: 3}, x => x === 4), undefined);
+assertEqual(findKey({a: 2, b: 2, c: 3}, x => x === 2), "a");
