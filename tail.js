dc6e56eb697464529577cbc55c1837a1453461d7
Alexander Evans <sander7989@gmail.com>
2023-07-06 22:44:47 -0600

    refactored tail function to require external function

--- tail.js
+++ tail.js
@@ -1,11 +1,4 @@
-const assertEqual = function(actual, expected) {
-  
-  if (actual === expected) {
-    console.log(`:D Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`:( Assertion Failed: ${actual} !== ${expected}`);
-  }
-};
+const assertEqual = require("./assertEqual");
 
 const tail = function(ar) {
   
@@ -18,15 +11,4 @@ const tail = function(ar) {
   return newArray;
 };
 
-let unchangedArray = [1, 2, 3, 4];
-
-tail(unchangedArray);
-
-assertEqual(unchangedArray.length, 4);
-
-assertEqual(tail([1, 2, 3, 4])[0], [2, 3, 4][0]);
-assertEqual(tail([1, 2, 3, 4])[1], [2, 3, 4][1]);
-assertEqual(tail([1, 2, 3, 4])[2], [2, 3, 4][2]);
-//assertEqual(tail(["asdasd", "dfgh", "srggh", 4]), ["dfgh", "srggh", 4]);
-//assertEqual(tail([4]), []);
-//assertEqual(tail([]), []);
+module.exports = tail;
