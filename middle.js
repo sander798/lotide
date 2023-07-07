b808dd9785da53e32b667d912c07dda8c333d3c4
Alexander Evans <sander7989@gmail.com>
2023-07-06 22:53:37 -0600

    refactor middle to use external functions

--- middle.js
+++ middle.js
@@ -1,27 +1,5 @@
-/*
-  Returns whether two given arrays are strictly equal.
-*/
-const eqArrays = function(ar1, ar2) {
-  if (ar1.length !== ar2.length){
-    return false;
-  }
-  
-  for (let i = 0; i < ar1.length; i++){
-    if (ar1[i] !== ar2[i]) {
-      return false;
-    }
-  }
-  
-  return true;
-};
-
-const assertArraysEqual = function(ar1, ar2) {
-  if (eqArrays(ar1, ar2) === true) {
-    console.log(`:D Assertion Passed: ${ar1} === ${ar2}`);
-  } else {
-    console.log(`:( Assertion Failed: ${ar1} !== ${ar2}`);
-  }
-};
+const eqArrays = require("./eqArrays");
+const assertArraysEqual = require("./assertArraysEqual");
 
 const middle = function(ar) {
   
@@ -34,9 +12,4 @@ const middle = function(ar) {
   return [ar[Math.ceil(ar.length / 2) - 1]];
 };
 
-assertArraysEqual(middle([1]), []); // => []
-assertArraysEqual(middle([1, 2]), []);// => []
-assertArraysEqual(middle([1, 2, 3]), [2]);
-assertArraysEqual(middle([1, 2, 3, 4, 5]), [3]);
-assertArraysEqual(middle([1, 2, 3, 4]), [2, 3]);
-assertArraysEqual(middle([1, 2, 3, 4, 5, 6]), [3, 4]);
+module.exports = middle;
