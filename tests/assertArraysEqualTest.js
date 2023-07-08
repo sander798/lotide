cbbbb8dd83489fe7290d6f39d977f03bce9f6f07
Alexander Evans <sander7989@gmail.com>
2023-07-07 22:55:09 -0600

    delete empty tests directory

--- tests/assertArraysEqualTest.js
+++
@@ -1,6 +0,0 @@
-const eqArrays = require("../eqArrays");
-const assertArraysEqual = require("../assertArraysEqual");
-
-assertArraysEqual([], []);
-assertArraysEqual([1, 2], [1, 2]);
-assertArraysEqual([], [2, 5]);
--- tests/eqArraysTest.js
+++
@@ -1,7 +0,0 @@
-const assertEqual = require("../assertEqual");
-const eqArrays = require("../eqArrays");
-
-assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true);
-assertEqual(eqArrays([1, 2, 3], [3, 2, 1]), true);
-assertEqual(eqArrays(["1", "2", "3"], ["1", "2", "3"]), true);
-assertEqual(eqArrays(["1", "2", "3"], ["1", "2", 3]), true);
