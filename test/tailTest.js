39d6aa53325887aaa4c2bc9c3938e9d354036855
Alexander Evans <sander7989@gmail.com>
2023-07-07 23:05:07 -0600

    add mocha tests to tailTest.js

--- test/tailTest.js
+++ test/tailTest.js
@@ -1,15 +1,37 @@
-const assertEqual = require("../assertEqual");
+//const assertEqual = require("../assertEqual");
 const tail = require("../tail");
+const assert = require('chai').assert;
 
-let unchangedArray = [1, 2, 3, 4];
+describe("#tail", () => {
+  it("Returns [2, 3] for [1, 2, 3]", () => {
+    assert.deepEqual(tail([1, 2, 3]), [2, 3]);
+  });
+  
+  it("Does not alter the array given as an argument.", () => {
+    let unchangedArray = [1, 2, 3];
+    tail(unchangedArray);
+    assert.deepEqual(unchangedArray, [1, 2, 3]);
+  });
+  
+  it("Returns [] for [1]", () => {
+    assert.deepEqual(tail([1]), []);
+  });
+  
+  it("Returns [] for []", () => {
+    assert.deepEqual(tail([]), []);
+  });
+  
+});
 
-tail(unchangedArray);
+//let unchangedArray = [1, 2, 3, 4];
 
-assertEqual(unchangedArray.length, 4);
+//tail(unchangedArray);
 
-assertEqual(tail([1, 2, 3, 4])[0], [2, 3, 4][0]);
-assertEqual(tail([1, 2, 3, 4])[1], [2, 3, 4][1]);
-assertEqual(tail([1, 2, 3, 4])[2], [2, 3, 4][2]);
+//assertEqual(unchangedArray.length, 4);
+
+//assertEqual(tail([1, 2, 3, 4])[0], [2, 3, 4][0]);
+//assertEqual(tail([1, 2, 3, 4])[1], [2, 3, 4][1]);
+//assertEqual(tail([1, 2, 3, 4])[2], [2, 3, 4][2]);
 //assertEqual(tail(["asdasd", "dfgh", "srggh", 4]), ["dfgh", "srggh", 4]);
 //assertEqual(tail([4]), []);
 //assertEqual(tail([]), []);
